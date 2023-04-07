01d1c0a44bf7f0ddab123ca0436aed2757ad1d25
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-04-07 22:47:31 +0700

    1 task

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
--- SQL-команды для создания таблиц
+--
+# SQL-команды для создания таблиц
+
+create table employees
+(
+  employee_id int PRIMARY KEY,
+  first_name varchar(100) not null,
+  last_name varchar(100) NOT NULL,
+  title varchar NOT NULL ,
+  birth_date date NOT NULL,
+  notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+     customer_id varchar PRIMARY KEY,
+	 company_name varchar NOT NULL,
+	 contact_name varchar NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar NOT NULL
+
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+
+with psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='Ane4ka18230'
+) as conn:
+    with conn.cursor() as cur:
+        with open("north_data/employees_data.csv") as file:
+            i = 1
+            reader = csv.reader(file)
+            for row in reader:
+                if '_' not in row[0]:
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                (i, row[0], row[1], row[2], row[3], row[4]))
+                    i += 1
+        with open("north_data/customers_data.csv") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if '_' not in row[0]:
+                    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                (row[0], row[1], row[2]))
+        with open("north_data/orders_data.csv") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if '_' not in row[0]:
+                    cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                                (row[0], row[1], row[2], row[3], row[4]))
+
+conn.close()
